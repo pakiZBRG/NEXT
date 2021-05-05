b6b4dbf992b2ad891061d233392a29db9bb0b15e
pakiZBRG <pavlovicnikola511@gmail.com>
2021-05-05 17:53:01 +0200

    Add more cards in separate file - data.js

--- components/Cards.js
+++ components/Cards.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Card from './Card';
+import { appartments } from '../data';
 
 const Cards = () => {
     return (
         <div className='flex justify-around'>
-            <Card title='Appartment#1' desc="Not so good appartment" imageUrl="http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"/>
+            {appartments.map(flat => 
+                <Card 
+                    key={flat.id} 
+                    title={flat.title} 
+                    desc={flat.desc} 
+                    imageUrl={flat.imageUrl}
+                />
+            )}
         </div>
     );
 }
---
+++ data.js
@@ -0,0 +1,38 @@
+export const appartments = [
+    {
+        id: 1,
+        title: "Appartment#1",
+        desc:"Not so good appartment",
+        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+    },
+    {
+        id: 2,
+        title: "Appartment#2",
+        desc:"Good appartment",
+        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+    },
+    {
+        id: 3,
+        title: "Appartment#3",
+        desc:"Somehwat good appartment",
+        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+    },
+    {
+        id: 4,
+        title: "Appartment#4",
+        desc:"Extraordinary good appartment",
+        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+    },
+    {
+        id: 5,
+        title: "Appartment#5",
+        desc:"Nicht gut Wohnung",
+        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+    },
+    {
+        id: 6,
+        title: "Appartment#6",
+        desc:"Not an appartment",
+        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+    }
+]
