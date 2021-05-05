2f54f558e00609c8430e90363e32fa7a5428e49c
pakiZBRG <pavlovicnikola511@gmail.com>
2021-05-05 18:05:50 +0200

    Add more info about appartments

--- components/Card.js
+++ components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({title, desc, imageUrl}) => {
     return (
         <div className='p-3 mb-6'>
-            <img className='rounded-3xl w-96 h-96 object-cover' src={imageUrl} alt={title}/>
+            <img className='rounded-3xl w-96 h-96 object-cover cursor-pointer' src={imageUrl} alt={title}/>
             <h1 className='font-extrabold text-xl py-2'>{title}</h1>
             <p className='text-gray-400'>{desc}</p>
         </div>
--- data.js
+++ data.js
@@ -3,36 +3,42 @@ export const appartments = [
         id: 1,
         title: "Appartment#1",
         desc:"Not so good appartment",
+        rating: 3.56,
         imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
     },
     {
         id: 2,
         title: "Appartment#2",
         desc:"Good appartment",
-        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+        rating: 4.11,
+        imageUrl:"https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
     },
     {
         id: 3,
         title: "Appartment#3",
         desc:"Somehwat good appartment",
-        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+        rating: 2.98,
+        imageUrl:"https://images.unsplash.com/photo-1489171078254-c3365d6e359f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80"
     },
     {
         id: 4,
         title: "Appartment#4",
         desc:"Extraordinary good appartment",
-        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+        rating: 3.25,
+        imageUrl:"https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1353&q=80"
     },
     {
         id: 5,
         title: "Appartment#5",
         desc:"Nicht gut Wohnung",
-        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+        rating: 4.02,
+        imageUrl:"https://images.unsplash.com/photo-1495433324511-bf8e92934d90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80"
     },
     {
         id: 6,
         title: "Appartment#6",
         desc:"Not an appartment",
-        imageUrl:"http://cdn.home-designing.com/wp-content/uploads/2019/01/Small-apartment-design.jpg"
+        rating: 3.90,
+        imageUrl:"https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1387&q=80"
     }
 ]
